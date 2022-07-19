bcb992c309b1188b89a5c9c6da4791b517f8b830
Muyiwaa <enochmwilliams@gmail.com>
2022-07-18 21:55:32 -0700

    fix linter issues

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    div = []
-    temp_result = 0
-    for i in range(0, list_length):
+    divisions = []
+    for index in range(list_length):
+        result = 0
         try:
-            temp_result = my_list[i] / my_list_2[i]
-        except TypeError:
-            temp_result = 0
-            print('wrong type')
+            result = my_list_1[index] / my_list_2[index]
         except ZeroDivisionError:
-            temp_result = 0
             print('division by 0')
+        except TypeError:
+            print('wrong type')
         except IndexError:
-            temp_result = 0
             print('out of range')
         finally:
-            pass
-        div.append(temp_result)
-    return div
+            divisions.append(result)
+
+    return divisions
